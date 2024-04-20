5e35dfc4930c0c324a053e75c734a8362e8392f5
ranjithkumar <ranjithkumar.m@codiis.com>
2024-04-20 23:23:48 +0530

    Added modification on build related issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,136 @@
 import Chart from "./page/Chart";
+import React from "react";
 import "./styles.css";
+import { FETCH } from "./service/utils/Fetch";
 
 export default function App() {
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [arrayX, setArrayX] = React.useState();
+  const [arrayY, setArrayY] = React.useState();
+  const [data, setData] = React.useState({
+    scatter: "",
+    line: "",
+    ground: ""
+  });
+
+
+  React.useEffect(() => {
+    fetchXdata()
+  }, []);
+
+  const fetchXdata = async () => {
+    setIsLoading(true);
+    try {
+      let response = await FETCH({
+        url: `gDa8uC/data`,
+        method: "GET",
+        authenticate: false,
+      });
+
+      if (response) {
+        setArrayX(
+          response.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0))
+        );
+        fetchYdata();
+      } else {
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error, "error");
+      setIsLoading(false)
+    }
+  };
+
+  const fetchYdata = async () => {
+    try {
+      let response = await FETCH({
+        url: "o5zMs5/data",
+        method: "GET",
+        authenticate: false,
+      });
+
+      if (response) {
+        await setArrayY(
+          response.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0))
+        );
+
+      } else {
+        setIsLoading(false)
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false)
+    }
+  };
+
+  const DataFuc = () => {
+    console.log(Array.isArray(arrayX), Array.isArray(arrayY));
+    if (Array.isArray(arrayX) && Array.isArray(arrayY)) {
+
+      let datasets = {
+        datasets: [{
+          label: "Scatter Plot (X,Y) Chart",
+          data: Array.from({ length: 50 }, (data, index) => ({
+            x: arrayX[index].RandomNumber,
+            y: arrayY[index].RandomNumber,
+          })),
+          backgroundColor: "rgba(255, 99, 132, 1)",
+        }]
+      };
+
+
+      let labels = Array.from({ length: 50 }, (data, index) => arrayX[index].id);
+
+      let linedata = {
+        labels,
+        datasets: [
+          {
+            label: 'X axis',
+            data: labels.map((i, index) => arrayX[index].RandomNumber),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+          {
+            label: 'Y axis',
+            data: labels.map((i, index) => arrayY[index].RandomNumber),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+        ],
+      };
+
+
+      let goundeddata = {
+        labels,
+        datasets: [
+          {
+            label: 'X axis',
+            data: labels.map((i, index) => arrayX[index].RandomNumber),
+            backgroundColor: 'rgb(255, 99, 132)',
+            stack: 'Stack 0',
+          },
+          {
+            label: 'Y axis',
+            data: labels.map((i, index) => arrayY[index].RandomNumber),
+            backgroundColor: 'rgb(75, 192, 192)',
+            stack: 'Stack 0',
+          },
+        ],
+      };
+      setData({ ...data, ["scatter"]: datasets, ['line']: linedata, ['ground']: goundeddata });
+      setIsLoading(false);
+    }
+
+  }
+
+  React.useEffect(() => {
+    DataFuc();
+  }, [arrayY]);
+
   return (
     <div className="App">
-      <Chart />
+      {isLoading ? <h1>Loading!....</h1> : <Chart data={data} />}
     </div>
   );
 }
--- src/service/utils/Fetch.js
+++ src/service/utils/Fetch.js
@@ -1,45 +1,43 @@
 export const FETCH = async (options) => {
-  
+
   const apiUrl = process.env.REACT_APP_API_URL;
 
-    let token = localStorage.getItem("token");
-    let response = {};
-    let { url, method, body, authenticate = true, authToken } = options;
-  
-    let headers = {
-      "Content-Type": "application/json",
-      'Access-Control-Allow-Origin':'*'
-    };
-  
-    if (authenticate) {
-      headers = { ...headers, Authorization: authToken ?? token };
-    }
-  
-    let requestOptions = { method, headers ,mode: 'cors'};
-  
-    if (method === "POST" || method === "PUT") {
-      requestOptions = { ...requestOptions, body: body };
-    }
-  
-    response = await fetch(apiUrl+url, requestOptions);
-    if (response.ok) {
-      return await response.json();
-    }
-  
-    if (response.status === 401) {
-      localStorage.removeItem("token");
-      window.location.pathname = "/login";
-    }
-  
-    let errorRes = await response.json();
-  
-    const responseError = {
-      type: "Error",
-      error: errorRes.error || errorRes.message || "Something went wrong",
-      success: errorRes.success || false,
-      invalid: errorRes.invalid || [],
-    };
-  
-    throw responseError;
+  let token = localStorage.getItem("token");
+  let response = {};
+  let { url, method, body, authenticate = true, authToken } = options;
+
+  let headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
   };
-  
+
+  if (authenticate) {
+    headers = { ...headers, Authorization: authToken ?? token };
+  }
+
+  let requestOptions = { method, headers, mode: 'cors' };
+
+  if (method === "POST" || method === "PUT") {
+    requestOptions = { ...requestOptions, body: body };
+  }
+
+  response = await fetch(apiUrl + url, requestOptions);
+  if (response.ok) {
+    return await response.json();
+  }
+
+  if (response.status === 401) {
+  }
+
+  let errorRes = await response.json();
+
+  const responseError = {
+    type: "Error",
+    error: errorRes.error || errorRes.message || "Something went wrong",
+    success: errorRes.success || false,
+    invalid: errorRes.invalid || [],
+  };
+
+  throw responseError;
+};
